f79d7bca80812354696fd4026d1587aa3ba8837c
Takashi-91 <madimetjaterencechuene@gmail.com>
2024-06-27 12:29:11 +0200

    Import Dependencies

---
+++ app/src/main/java/com/example/navroutefinder/ui/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.navroutefinder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navroutefinder.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
